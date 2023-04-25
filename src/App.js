7463f985f35421ed0795dd4aff8ace13bb797177
Juram Lee <juram2@naver.com>
2023-04-25 14:27:42 +0900

    :art:CSS: header with only logo. the logo will be replaced later

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/routing';
+import GlobalStyles from 'Styles/global';
 
 function App() {
-	return <RouterProvider router={router} />;
+	return (
+		<>
+			<GlobalStyles />
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
---
+++ src/Styles/theme.js
@@ -0,0 +1,21 @@
+// 폰트 크기
+const fontSize = {
+	xs: '12px',
+	sm: '14px',
+	base: '16px',
+	lg: '28px',
+	xl: '32px',
+};
+
+// 폰트 굵기
+const fontWeight = {
+	light: '300',
+	regular: '400',
+	bold: '700',
+	bolder: '900',
+};
+
+export const theme = {
+	fontSize,
+	fontWeight,
+};
