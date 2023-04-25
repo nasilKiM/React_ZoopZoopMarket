48b6ccae842c2160f658bddb14f5fde64036d5be
nasilKiM <nasikun@naver.com>
2023-04-25 22:46:38 +0900

    FEAT:ADD:Heart-btn component

---
+++ src/Components/Buttons/HeartBtn/HeartBtn.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+function HeartBtn() {
+	const [active, setActive] = useState(false);
+
+	const handleClick = () => {
+		setActive(!active);
+	};
+
+	return <S.Button className={active ? 'active' : ''} onClick={handleClick} />;
+}
+
+export default HeartBtn;
+
+const Button = styled.button`
+	height: 20px;
+	width: 20px;
+	background-color: transparent;
+	border: none;
+	cursor: pointer;
+	display: inline-block;
+	background-image: url('Assets/Icon/heart.png');
+	background-repeat: no-repeat;
+	background-size: contain;
+
+	&.active {
+		background-image: url('Assets/Icon/heart-filled.png');
+	}
+`;
+
+const S = {
+	Button,
+};
