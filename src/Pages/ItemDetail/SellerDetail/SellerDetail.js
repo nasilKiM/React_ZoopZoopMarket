8eb61d3c8f5e2cbca69eeeb1251f2ae20cb9927a
YoungSeungJang <1345620@naver.com>
2023-04-26 09:36:42 +0900

    :sparkles:Feat: BuyerDetail&SellerDetail
    BuyerDetail&SellerDetail보완, 겹치는 부분(detailHead)컴포넌트로 분리

--- src/Pages/ItemDetail/SellerDetail/SellerDetail.js
+++ src/Pages/ItemDetail/SellerDetail/SellerDetail.js
@@ -1,25 +1,31 @@
 import styled from 'styled-components';
 import DetailHead from '../Components/DetailHead/detailHead';
 import DetailContent from '../Components/DetailContent/detailContent';
+import { flexAllCenter } from 'Styles/common';
+import { useState } from 'react';
 
 const SellerDetailPage = ({ state }) => {
+	const [detailState, setDetailState] = useState(true);
 	return (
 		<S.Wrapper>
 			<div>판매자페이지</div>
-			<div>
-				<div>판매중</div>
+			<S.EditBar>
+				<select>
+					<option>판매중</option>
+					<option>판매완료</option>
+				</select>
 				<ul>
 					<li>Edit</li>
 					<li>Hide</li>
 					<li>Delete</li>
 				</ul>
-			</div>
+			</S.EditBar>
 			<DetailHead />
-			<div>
-				<div>상세정보</div>
-				<div>채팅 내역</div>
-			</div>
-			<DetailContent state={state} />
+			<S.DetailAndChatBar>
+				<div onClick={() => setDetailState(true)}>상세정보</div>
+				<div onClick={() => setDetailState(false)}>채팅 내역</div>
+			</S.DetailAndChatBar>
+			{detailState ? <DetailContent state={state} /> : <div>채팅</div>}
 		</S.Wrapper>
 	);
 };
@@ -32,6 +38,48 @@ const Wrapper = styled.div`
 	margin: 0 auto;
 `;
 
+const EditBar = styled.div`
+	font-size: ${({ theme }) => theme.fontSize.lg};
+	${flexAllCenter}
+	justify-content: space-between;
+	& > select {
+		font-size: ${({ theme }) => theme.fontSize.lg};
+		padding: 10px;
+	}
+	& > ul {
+		${flexAllCenter}
+		&>li {
+			margin: 0 10px;
+		}
+		& > li::after {
+			content: '|';
+			margin-left: 10px;
+		}
+		& > li:last-child::after {
+			content: '';
+		}
+	}
+`;
+
+const DetailAndChatBar = styled.div`
+	${flexAllCenter}
+	&>div {
+		${flexAllCenter}
+		border-top: 1px solid;
+		border-bottom: 1px solid;
+		padding: 20px;
+		font-size: ${({ theme }) => theme.fontSize.lg};
+		font-weight: ${({ theme }) => theme.fontWeight.bold};
+		letter-spacing: 5px;
+		width: 100%;
+	}
+	& > div:first-child {
+		border-right: 1px solid;
+	}
+`;
+
 const S = {
 	Wrapper,
+	EditBar,
+	DetailAndChatBar,
 };
