31904a0a213b2f52ec3fbedd9f99dd384e0822f9
seonyeong719 <sy904280@gmail.com>
2023-04-27 20:47:15 +0900

    :lipstick:Design: searchBar 모바일 버전 만드는중

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './Routes/routing';
+// import { RouterProvider } from 'react-router-dom';
+// import router from './Routes/routing';
 import GlobalStyles from 'Styles/global';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './Styles/theme';
+import MobileSearchBar from 'Components/SearchBar/Mobile/MobileSearchBar';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<RouterProvider router={router} />
+			{/* <RouterProvider router={router} /> */}
+			<MobileSearchBar />
 		</ThemeProvider>
 	);
 }
