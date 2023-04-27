385af72b65ee879db98b17d1110b726b75786f64
JAE HOON LEE <jazzyrain722@naver.com>
2023-04-28 01:11:25 +0900

    :zap:Swiper edit complete

--- src/Pages/Main/Components/preview.js
+++ src/Pages/Main/Components/preview.js
@@ -11,10 +11,17 @@ const Preview = ({ categoryData, userLocation, userName }) => {
 			: `${userName}님 주변의 무료나눔 물품들 이에요!`;
 
 	const itemList = [1, 2, 3, 4, 5, 6, 7, 8];
-	const [swipe, setSwipe] = useState();
-	const swiperStyle = {
-		width: '100%',
+
+	const [swiper, setSwiper] = useState(null);
+
+	const handleNext = () => {
+		swiper.slideNext();
 	};
+
+	const handlePrev = () => {
+		swiper.slidePrev();
+	};
+
 	return (
 		<S.Wrapper>
 			<S.UpperSwiper>
@@ -23,21 +30,16 @@ const Preview = ({ categoryData, userLocation, userName }) => {
 				<S.More> 더보기 &gt; </S.More>
 			</S.UpperSwiper>
 			<S.SwiperWrapper>
-				<Swiper
-					onBeforeInit={swipper => setSwipe(swipper)}
-					style={swiperStyle}
-					spaceBetween={0}
-					slidesPerView={4}
-					cssMode
-				>
+				<S.Btn onClick={handlePrev}> &lt;</S.Btn>
+				<Swiper onSwiper={setSwiper} spaceBetween={0} slidesPerView={4}>
 					{itemList.map(item => (
 						<SwiperSlide>
 							<ItemCard key={item} />
 						</SwiperSlide>
 					))}
 				</Swiper>
-				<button onClick={() => swipe?.slidePrev()}>전</button>
-				<button>후</button>
+
+				<S.Btn onClick={handleNext}> &gt;</S.Btn>
 			</S.SwiperWrapper>
 		</S.Wrapper>
 	);
@@ -56,39 +58,46 @@ const UpperSwiper = styled.div`
 	width: 100%;
 	height: 50px;
 	display: flex;
-	border: 5px solid navajowhite;
 `;
 const CategoryBox = styled.div`
 	width: 20%;
 	text-align: center;
-	padding-top: 10px;
+	padding-top: 15px;
 	font-size: ${({ theme }) => theme.fontSize.base};
 	font-weight: ${({ theme }) => theme.fontWeight.bolder};
-
 	background-color: ${({ theme }) => theme.color.primary};
 	color: ${({ theme }) => theme.color.white};
 	border-radius: 10px;
 `;
 const CategoryText = styled.div`
-	padding-top: 10px;
+	padding-top: 15px;
 	font-size: ${({ theme }) => theme.fontSize.base};
-	border: 1px solid black;
 	margin: 0 5%;
 `;
 const More = styled.div`
-	padding-top: 10px;
+	padding-top: 15px;
 	width: 55%;
 	font-size: ${({ theme }) => theme.fontSize.base};
-	border: 1px solid black;
+
 	text-align: end;
 `;
 const SwiperWrapper = styled.div`
-	padding: 10px 60px;
-	border: 3px solid ${({ theme }) => theme.color.subBeigeGreen};
-	margin-top: 20px;
+	padding: 10px 30px;
+	border: 1px solid ${({ theme }) => theme.color.subBeigeGreen};
+	margin-top: 10px;
+	display: flex;
+	align-items: center;
+`;
+const Btn = styled.button`
+	border: none;
+	font-size: ${({ theme }) => theme.fontSize.xl};
+	background: none;
+	width: 70px;
+	:hover {
+		color: ${({ theme }) => theme.color.primary};
+	}
 `;
 
-const PrevBtn = styled.div``;
 const S = {
 	Wrapper,
 	UpperSwiper,
@@ -96,4 +105,5 @@ const S = {
 	CategoryText,
 	More,
 	SwiperWrapper,
+	Btn,
 };
