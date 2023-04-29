a28cc37611efd6b27fb7a45de87c3d8ad25c2c6d
nasilKiM <nasikun@naver.com>
2023-04-29 21:41:14 +0900

    :lipstick: 카드 거래완료 모바일버전  컴포턴트 ui수정

--- src/Components/Card/Mobile/MobileCardSoldout.js
+++ src/Components/Card/Mobile/MobileCardSoldout.js
@@ -1,83 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SoldoutCard = () => {
+const MobileSoldoutCard = () => {
 	return (
 		<S.Wrapper>
 			<S.Container>
-				<div>
-					<S.Button>거래완료</S.Button>
-					<S.ItemImg src="Assets/Images/bicycle.jpg" />
-					<S.ItemInfo>
-						<S.ItemTitle>[제목] 제목이 들어갑니다.</S.ItemTitle>
-						<S.ItemPrice>130,000 원</S.ItemPrice>
-						<S.ItemTag>#태그 #태그2 #태그3</S.ItemTag>
-					</S.ItemInfo>
-				</div>
+				<S.Button>거래완료</S.Button>
+				<S.ItemImg src="Assets/Images/bicycle.jpg" />
+				<S.ItemInfo>
+					<S.ItemTitle>[제목] 제목이 들어갑니다.</S.ItemTitle>
+					<S.ItemPrice>130,000 원</S.ItemPrice>
+					<S.ItemTag>#태그 #태그2 #태그3</S.ItemTag>
+				</S.ItemInfo>
 			</S.Container>
 		</S.Wrapper>
 	);
 };
 
-export default SoldoutCard;
+export default MobileSoldoutCard;
 
 const Wrapper = styled.div`
 	display: flex;
 `;
 
 const Button = styled.span`
+	position: absolute;
+	padding: 10px;
 	text-align: center;
 	background-color: white;
-	padding: 10px;
 	font-size: ${({ theme }) => theme.fontSize.base};
-	font-weight: ${({ theme }) => theme.fontWeight.bold};
-	position: absolute;
+	font-weight: ${({ theme }) => theme.fontWeight.bolder};
 	color: red;
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%);
+	z-index: 10000;
 `;
 
 const Container = styled.div`
 	position: relative;
-	min-width: 200px;
-	max-height: 400px;
-	margin: 10px;
-	& div {
-		background-color: #808080;
-		opacity: 0.5;
-	}
+	display: flex;
+	min-width: 400px;
+	max-height: 100px;
+	border-radius: 5px;
+	background-color: #808080;
+	opacity: 0.5;
 `;
 
 const ItemImg = styled.img`
-	width: 100%;
-	padding: 15px;
-	max-height: 250px;
+	min-width: 90px;
+	max-height: 90px;
+	padding: 10px;
 	object-fit: cover;
-	margin: auto;
 `;
 
 const ItemInfo = styled.div`
-	padding-top: 15px;
-	max-height: 150px;
+	padding-top: 10px;
+	padding-left: 10px;
+	max-height: 100px;
 	display: flex;
 	flex-direction: column;
-	padding: 0 15px;
 `;
 
 const ItemTitle = styled.div`
-	font-size: ${({ theme }) => theme.fontSize.base};
+	font-size: ${({ theme }) => theme.fontSize.sm};
 	margin-bottom: 10px;
 `;
 
 const ItemPrice = styled.span`
-	font-size: ${({ theme }) => theme.fontSize.base};
+	font-size: ${({ theme }) => theme.fontSize.sm};
 	font-weight: ${({ theme }) => theme.fontWeight.bold};
 	margin-bottom: 15px;
 `;
 
 const ItemTag = styled.span`
-	font-size: ${({ theme }) => theme.fontSize.sm};
+	font-size: ${({ theme }) => theme.fontSize.xs};
 	overflow: hidden;
 	margin-bottom: 10px;
 `;
--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -1,4 +1,3 @@
-import SoldoutCard from 'Components/Card/Desktop/CardSoldout';
 import BasicFooter from 'Components/Layout/Footer';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -10,7 +9,6 @@ const LandingPage = () => {
 				<S.LandingHeader>
 					<S.LogoImage src="Assets/web_logo.png" />
 				</S.LandingHeader>
-				<SoldoutCard />
 				<S.Section1>
 					<marquee>
 						- ZOOP ZOOP MARKET - ZOOP ZOOP MARKET- ZOOP ZOOP MARKET- ZOOP ZOOP
