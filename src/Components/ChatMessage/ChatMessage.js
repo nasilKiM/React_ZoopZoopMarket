e045fe5054fbe5d64a48c9c06e59f5bbec3560b5
nasilKiM <nasikun@naver.com>
2023-05-01 19:53:03 +0900

    :sparkles:Edit: chatMessage 컴포넌트 데이터 수정

--- src/Components/ChatMessage/ChatMessage.js
+++ src/Components/ChatMessage/ChatMessage.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-const ChatMessage = () => {
+const ChatMessage = ({ chat, onClick }) => {
 	return (
 		<>
-			<S.ChatContent>
+			<S.ChatContent onClick={onClick}>
 				<img src="Assets/Images/bicycle.jpg" />
 				<div>
 					<S.ChatContentUpper>
-						<S.NickName>닉네임</S.NickName>
-						<span>채팅시간</span>
+						<S.NickName>{chat.user.nickname}</S.NickName>
+						<span>{chat.msg.createdAt}</span>
 					</S.ChatContentUpper>
 					<S.ChatContentdown>
-						<span>마지막 채팅 내용</span>
+						<span>{chat.msg.content}</span>
 					</S.ChatContentdown>
 				</div>
 			</S.ChatContent>
