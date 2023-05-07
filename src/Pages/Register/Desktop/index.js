7b87d9e5f9762b453ec38a26b2530a6873998d9f
JAE HOON LEE <jazzyrain722@naver.com>
2023-05-08 00:12:50 +0900

    :sparkles:Price 창 세자리 끊기

--- src/Pages/Register/Desktop/index.js
+++ src/Pages/Register/Desktop/index.js
@@ -8,6 +8,34 @@ import { Axios } from 'Apis/@core';
 
 const RegisterPage = () => {
 	const [searchResult, setSearchResult] = useState('');
+
+	const [price, setPrice] = useState('');
+	const [tags, setTags] = useState([]);
+
+	const handleKeyDown = e => {
+		if (e.keyCode === 13) {
+			// 엔터키 눌림.
+			e.preventDefault();
+			const newTag = e.target.value.trim(); //공백있으면 trim으로 제거.
+			if (newTag) {
+				setTags([...tags, newTag]);
+				e.target.value = ''; // 입력창 초기화
+			}
+		}
+	};
+	const handleDelete = e => {
+		console.log('!!!!!!', e);
+		//setTags(prevTags => prevTags.filter(tag => tag !== tagToRemove)); //tagToRemove와 일치하지 않는 모든 요소를 포함하는 새로운 배열을 반환
+		//() => handleDelete(tag)와 같이 콜백 함수 형태로 작성하여, 클릭 이벤트가 발생했을 때에만 함수가 실행되도록
+	};
+
+	const handlePriceChange = e => {
+		const value = e.target.value;
+		const num = parseInt(value.replace(/[^0-9]/g, ''), 10);
+		const priceValue = isNaN(num) ? 0 : num;
+		const formattedPrice = priceValue.toLocaleString();
+		setPrice(formattedPrice);
+	};
 	const {
 		register,
 		handleSubmit,
@@ -22,8 +50,9 @@ const RegisterPage = () => {
 			formData.append('category', Number(data.price) === 0 ? 1 : 0);
 			formData.append('description', data.content);
 			formData.append('region', searchResult);
-			formData.append('tag', [data.tag]);
-			formData.append('images', data.mainImg);
+			formData.append('tag', data.tag);
+			console.log('>>>>>>>>>>>>>', data.mainImg);
+			formData.append('images', JSON.stringify(data.mainImg));
 			Axios.post('/api/product', formData);
 			alert('물품등록이 완료되었습니다.');
 		} catch (err) {
@@ -62,6 +91,9 @@ const RegisterPage = () => {
 								message: '숫자만 입력해주세요',
 							},
 						})}
+						value={price}
+						type="text"
+						onChange={handlePriceChange}
 					></S.InputBox>
 					{errors.price && <Error role="alert">{errors.price.message}</Error>}
 				</S.InputContainer>
@@ -73,10 +105,24 @@ const RegisterPage = () => {
 					<S.InputBox
 						placeholder="#재훈이네 #금쪽이 #이재훈"
 						{...register('tag', {
-							required: '최소 1개 이상 입력해주세요.',
+							required: '1개이상 꼭 입력해주세요.',
 						})}
+						value={tags}
+					></S.InputBox>
+					<S.InputBox
+						placeholder="이곳에 입력해주세요."
+						onKeyDown={handleKeyDown}
 					></S.InputBox>
 					{errors.tag && <Error role="alert">{errors.tag.message}</Error>}
+					<S.TagWrapper>
+						{tags &&
+							tags.map((tag, index) => (
+								<S.TagBox key={index}>
+									{tag}
+									<button onClick={e => handleDelete(e)}>x</button>
+								</S.TagBox>
+							))}
+					</S.TagWrapper>
 				</S.InputContainer>
 			</S.Line>
 			<S.AddressWrapper>
@@ -257,6 +303,14 @@ const RegisterBtn = styled.button`
 	}
 `;
 
+const TagWrapper = styled.div`
+	border: 2px solid green;
+`;
+
+const TagBox = styled.div`
+	border: 1px solid green;
+`;
+
 const S = {
 	Wrapper,
 	Blank,
@@ -275,4 +329,6 @@ const S = {
 	ContentBox,
 	TxtArea,
 	SearchBtn,
+	TagWrapper,
+	TagBox,
 };
