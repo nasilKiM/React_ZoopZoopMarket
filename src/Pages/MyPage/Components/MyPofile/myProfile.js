90715b6ea84a9263972b5429a58514af2d772f98
YoungSeungJang <1345620@naver.com>
2023-05-10 18:09:08 +0900

    :sparkles:Feat: 마이페이지 로그아웃

--- src/Pages/MyPage/Components/MyPofile/myProfile.js
+++ src/Pages/MyPage/Components/MyPofile/myProfile.js
@@ -1,12 +1,15 @@
 import UserApi from 'Apis/userApi';
 import MannerMeter from 'Components/Icon/Icon';
 import Profile from 'Components/Profile/Desktop/profile';
+import TokenService from 'Repository/TokenService';
 
 import { flexAllCenter } from 'Styles/common';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const MyProfile = ({ userInfo }) => {
+	const navigate = useNavigate();
 	const [myProfile, setMyProfile] = useState();
 	useEffect(() => {
 		const getProfile = async () => {
@@ -16,6 +19,19 @@ const MyProfile = ({ userInfo }) => {
 		};
 		getProfile();
 	}, []);
+
+	const onClickLogOutBtn = async () => {
+		try {
+			const res = await UserApi.logout();
+			if (res.status === 200) {
+				TokenService.removeToken();
+				alert('로그아웃 되었습니다');
+				navigate('/');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	return (
 		<S.Wrapper>
@@ -30,7 +46,7 @@ const MyProfile = ({ userInfo }) => {
 				<div>내 지역</div>
 			</div>
 			<div>
-				<div>로그아웃</div>
+				<div onClick={onClickLogOutBtn}>로그아웃</div>
 			</div>
 		</S.Wrapper>
 	);
