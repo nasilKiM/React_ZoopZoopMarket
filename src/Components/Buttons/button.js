2813b64eb56dd15022f80f870250b66d6c520ad6
seonyeong719 <sy904280@gmail.com>
2023-05-17 23:51:18 +0900

    :lipstick:Design: form 페이지 변경 css 완료

--- src/Components/Buttons/button.js
+++ src/Components/Buttons/button.js
@@ -1,10 +1,12 @@
 import * as S from './style';
- 
+
 function CustomButton(props) {
-    const {variant, shape, size, children, ...rest} = props;
-    return (
-        <S.PropsBtn variant={variant} shape={shape} size={size} {...rest}>{children}</S.PropsBtn>
-    );
+	const { variant, shape, size, children, ...rest } = props;
+	return (
+		<S.PropsBtn variant={variant} shape={shape} size={size} {...rest}>
+			{children}
+		</S.PropsBtn>
+	);
 }
 
 export default CustomButton;
