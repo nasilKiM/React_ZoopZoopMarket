0bdf796a6d66804d996d427dc450b0657c38969f
seonyeong719 <sy904280@gmail.com>
2023-05-18 17:34:55 +0900

    :sparkles:Feat: css수정, 기능 보충

--- src/Components/Buttons/button.js
+++ src/Components/Buttons/button.js
@@ -3,7 +3,13 @@ import * as S from './style';
 function CustomButton(props) {
 	const { variant, shape, size, children, ...rest } = props;
 	return (
-		<S.PropsBtn variant={variant} shape={shape} size={size} {...rest}>
+		<S.PropsBtn
+			variant={variant}
+			shape={shape}
+			size={size}
+			disabled={!!rest.disabled}
+			{...rest}
+		>
 			{children}
 		</S.PropsBtn>
 	);
