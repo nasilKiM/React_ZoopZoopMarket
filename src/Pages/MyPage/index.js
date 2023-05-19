0db02cf974103a70c7498c67a4ce82abd2af158d
hyunseoKoo <koohs414@naver.com>
2023-05-20 07:43:59 +0900

    :lipstick: Update: 마이페이지 메뉴바 디자인 변경

--- src/Pages/MyPage/index.js
+++ src/Pages/MyPage/index.js
@@ -2,25 +2,15 @@ import styled from 'styled-components';
 import MyProfile from './Components/MyPofile/myProfile';
 import ToggleBar from 'Components/Toggle/Toggle';
 import { Outlet } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 
 const MyPage = () => {
-	const isDesktopOrMobile = useMediaQuery({query: '(max-width: 768px'});
 	return (
 		<>
-		{isDesktopOrMobile !== true ?
 		<S.Wrapper>
 			<MyProfile/>
 			<ToggleBar/>
 			<Outlet />
 		</S.Wrapper>
-		:
-		<S.MWrapper>
-			<MyProfile/>
-			<ToggleBar/>
-			<Outlet />
-		</S.MWrapper>
-		}
 		</>
 	);
 };
@@ -28,16 +18,10 @@ const MyPage = () => {
 export default MyPage;
 
 const Wrapper = styled.div`
-	width: 60%;
-	margin: 0 auto;
-`;
-
-const MWrapper = styled.div`
-	width: 414px;
+	width: 70%;
 	margin: 0 auto;
 `;
 
 const S = {
 	Wrapper,
-	MWrapper
 };
