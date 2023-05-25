054a00603049db268e85a527a45e415295334219
hyunseoKoo <koohs414@naver.com>
2023-05-25 21:28:26 +0900

    :lipstick: Update: 가계부 디자인 수정

--- src/Pages/MyPage/MyAccountBook/Desktop/index.js
+++ src/Pages/MyPage/MyAccountBook/Desktop/index.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const AccountBookPage = () => {
 	const [date, setDate] = useState(new Date());
-	const [category, setCategory] = useState();
+	const [category, setCategory] = useState(); // params_category
 	const [year, setYear] = useState(date.getFullYear());
 	const [month, setMonth] = useState(date.getMonth()+1);
 
@@ -14,8 +14,7 @@ const AccountBookPage = () => {
 
 	const monthFirstDate = `${year}-${month}-1`; // params_start
 	const monthLastDate = `${year}-${month}-${lastDay}`; // params_end
-	console.log(monthFirstDate, monthLastDate);
-
+	console.log(monthFirstDate, monthLastDate); // 확인용
 
 	return(
 	  <S.Wrapper>
