d888f8994fbd080395275f0fb5c5a2600b8ed9ae
nasilKiM <nasikun@naver.com>
2023-06-01 00:47:55 +0900

    :art: Edit : 에러페이지 반응형 추가

--- src/Error/Desktop/index.js
+++ src/Error/Desktop/index.js
@@ -22,9 +22,15 @@ export default DesktopError;
 
 const Wrapper = styled.div`
 	width: 70%;
+	min-width: 414px;
 	max-width: 1200px;
-	min-width: 700px;
 	margin: 0 auto;
+	@media (max-width: 700px) {
+		width: 95%;
+	}
+	@media (max-width: 800px) {
+		width: 90%;
+	}
 `;
 
 const ImageContainer = styled.div`
@@ -51,7 +57,7 @@ const Emphasize = styled.div`
 	font-size: ${({ theme }) => theme.fontSize.big};
 `;
 const ButtonBox = styled.div`
-	width: 40%;
+	width: 50%;
 	margin: 0 auto;
 	padding: 10px;
 	display: flex;
