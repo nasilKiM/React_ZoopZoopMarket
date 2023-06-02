68a5dfc31d06855f2432d06fc535a2ee84a41e24
seonyeong719 <sy904280@gmail.com>
2023-06-02 22:21:35 +0900

    :recycle:Refactor: 쿼리 적용중

--- src/Pages/MyPage/Components/MyPofile/myProfile.js
+++ src/Pages/MyPage/Components/MyPofile/myProfile.js
@@ -1,33 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
 
 import UserApi from 'Apis/userApi';
-import MyPageApi from 'Apis/myPageApi';
 
 import MannerMeter from 'Components/Icon/Icon';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
+import useUserInfo from 'Hooks/Queries/get-user-info';
+import useMyUserInfo from 'Hooks/Queries/get-mypage-mainPage';
+import { useMutation } from '@tanstack/react-query';
 
 const MyProfile = () => {
-	const [userInfo, setUserInfo] = useState('');
-	const [userProfile, setUserProfile] = useState('');
 	const [profileImg, setProfileImg] = useState();
 	const photoInput = useRef();
+	const { data, refetch, isLoading } = useUserInfo();
+	const { data: myData } = useMyUserInfo();
 
-	const { data } = useQuery(['userInfo'], () => UserApi.userInfo());
+	const { mutate } = useMutation(
+		formData => UserApi.userProfileEdit(formData),
+		{
+			onSuccess: () => {
+				refetch();
+			},
+		},
+	);
 
-	const getUserProfile = async () => {
-		try {
-			const res = await MyPageApi.myMainPage();
-			setUserProfile(res);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const profileImgEdit = async e => {
+	const profileImgEdit = e => {
+		if (!e.target.files[0]) return;
 		const formData = new FormData();
 		const file = e.target.files[0];
 		formData.append('profile_url', file);
@@ -35,100 +35,93 @@ const MyProfile = () => {
 		const imageUrl = URL.createObjectURL(file);
 		setProfileImg(imageUrl);
 
-		try {
-			await UserApi.userProfileEdit(formData);
-		} catch (error) {
-			console.log(error);
-		}
+		mutate(formData);
 	};
 
 	const handleClick = () => {
 		photoInput.current.click();
 	};
 
-	useEffect(() => {
-		getUserProfile();
-		setUserInfo(data);
-	}, [profileImg, data]);
-
-	const { User, ondo } = userProfile && userProfile.data;
-
 	return (
-		<S.Wrapper>
-			{userInfo && userProfile && (
-				<S.Info>
-					<S.ImgWrap>
-						<S.Img
-							src={
-								userInfo.data.profile_url
-									? profileImg
-										? profileImg
-										: userInfo.data.profile_url
-									: '/Assets/Images/기본 프로필.png'
-							}
-						/>
-						<S.ProfileImg>
-							<S.FontAwesomeIconImg
-								icon={faCamera}
-								style={{ color: '#ffffff', fontSize: '15px' }}
-								onClick={handleClick}
-							/>
-							<input
-								type="file"
-								accept="image/jpg, image/jpeg, image/png"
-								multiple
-								ref={photoInput}
-								style={{ display: 'none' }}
-								onChange={e => profileImgEdit(e)}
-							/>
-						</S.ProfileImg>
-					</S.ImgWrap>
-					<S.DetailWrapper>
-						<S.Detail>
-							<S.List>
-								<S.InfoTitle>닉네임</S.InfoTitle>
-								{User && <S.InfoContent>{User.nickName}</S.InfoContent>}
-							</S.List>
-							<S.List>
-								<S.InfoTitle>매너온도</S.InfoTitle>
-								<S.InfoContent>
-									<MannerMeter ondo={ondo} />
-								</S.InfoContent>
-							</S.List>
-							<S.List>
-								<S.InfoTitle>활동지역</S.InfoTitle>
-								<S.InfoContent>#{data.data.region}</S.InfoContent>
-							</S.List>
-						</S.Detail>
-						<S.Detail>
-							<S.List>
-								<S.InfoTitle>내 등록템</S.InfoTitle>
-								<S.InfoContent>
-									<span>
-										{userProfile.data.productsCount
-											? userProfile.data.productsCount
-											: 0}
-									</span>{' '}
-									개
-								</S.InfoContent>
-							</S.List>
-							<S.List>
-								<S.InfoTitle>내 관심템</S.InfoTitle>
-								<S.InfoContent>
-									<span>{userProfile.data.likeCount}</span> 개
-								</S.InfoContent>
-							</S.List>
-							<S.List>
-								<S.InfoTitle>채팅</S.InfoTitle>
-								<S.InfoContent>
-									<span>{userProfile.data.chatCount}</span> 건
-								</S.InfoContent>
-							</S.List>
-						</S.Detail>
-					</S.DetailWrapper>
-				</S.Info>
+		<>
+			{!isLoading && (
+				<S.Wrapper>
+					{data && myData && (
+						<S.Info>
+							<S.ImgWrap>
+								<S.Img
+									src={
+										data.profile_url
+											? profileImg
+												? profileImg
+												: data.profile_url
+											: '/Assets/Images/기본 프로필.png'
+									}
+								/>
+								<S.ProfileImg>
+									<S.FontAwesomeIconImg
+										icon={faCamera}
+										style={{ color: '#ffffff', fontSize: '15px' }}
+										onClick={handleClick}
+									/>
+									<input
+										type="file"
+										accept="image/jpg, image/jpeg, image/png"
+										multiple
+										ref={photoInput}
+										style={{ display: 'none' }}
+										onChange={e => profileImgEdit(e)}
+									/>
+								</S.ProfileImg>
+							</S.ImgWrap>
+							<S.DetailWrapper>
+								<S.Detail>
+									<S.List>
+										<S.InfoTitle>닉네임</S.InfoTitle>
+										{myData.User && (
+											<S.InfoContent>{myData.User.nickName}</S.InfoContent>
+										)}
+									</S.List>
+									<S.List>
+										<S.InfoTitle>매너온도</S.InfoTitle>
+										<S.InfoContent>
+											<MannerMeter ondo={myData.ondo} />
+										</S.InfoContent>
+									</S.List>
+									<S.List>
+										<S.InfoTitle>활동지역</S.InfoTitle>
+										<S.InfoContent>#{data.region}</S.InfoContent>
+									</S.List>
+								</S.Detail>
+								<S.Detail>
+									<S.List>
+										<S.InfoTitle>내 등록템</S.InfoTitle>
+										<S.InfoContent>
+											<span>
+												{myData.productsCount ? myData.productsCount : 0}
+											</span>{' '}
+											개
+										</S.InfoContent>
+									</S.List>
+									<S.List>
+										<S.InfoTitle>내 관심템</S.InfoTitle>
+										<S.InfoContent>
+											<span>{myData.likeCount}</span> 개
+										</S.InfoContent>
+									</S.List>
+									<S.List>
+										<S.InfoTitle>채팅</S.InfoTitle>
+										<S.InfoContent>
+											<span>{myData.chatCount}</span> 건
+										</S.InfoContent>
+									</S.List>
+								</S.Detail>
+							</S.DetailWrapper>
+						</S.Info>
+					)}
+				</S.Wrapper>
 			)}
-		</S.Wrapper>
+		</>
 	);
 };
 
