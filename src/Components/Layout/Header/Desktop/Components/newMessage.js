e2574a6f0c894f4fc8820e46cc5eded9a802e191
YoungSeungJang <50819030+YoungSeungJang@users.noreply.github.com>
2023-06-05 08:50:03 +0900

    :sparkles:Feat: chat
    채팅목록 last message 길이길면 ...으로 표시하도록 수정

--- src/Components/Layout/Header/Desktop/Components/newMessage.js
+++ src/Components/Layout/Header/Desktop/Components/newMessage.js
@@ -35,15 +35,15 @@ const NewMessage = ({ popupMsg, setPopupMsg }) => {
 	}
 
 	return (
-		<S.Wrapper onClick={handleClickPopupMsg}>
-			<div>
-				<img src={itemInfo?.searchProduct.img_url} />
-			</div>
-			<div>
-				<div>상품명 : {itemInfo?.searchProduct.title}</div>
-				<div>{popupMsg.nickName} 님의 새로운 메세지</div>
-			</div>
-		</S.Wrapper>
+				<S.Wrapper onClick={handleClickPopupMsg}>
+					<div>
+						<img src={itemInfo?.searchProduct.img_url} />
+					</div>
+					<div>
+						<div>상품명 : {itemInfo?.searchProduct.title}</div>
+						<div>{popupMsg.nickName} 님의 새로운 메세지</div>
+					</div>
+				</S.Wrapper>
 	);
 };
 
@@ -55,7 +55,7 @@ const Wrapper = styled.div`
 	justify-content: space-around;
 	position: absolute;
 	top: 200px;
-	z-index: 999;
+	z-index: 99;
 	width: 500px;
 	height: 150px;
 	border-radius: 20px;
